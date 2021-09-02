0a79b2813e76949aae53e99fa5f498e69e946049
bki123 <41273107+crazybki@users.noreply.github.com>
2021-09-02 21:28:57 +0200

    Added search functionality

--- src/components/frontpage/FetchHotels.js
+++ src/components/frontpage/FetchHotels.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useState, useEffect } from "react";
 import { hotelApi } from "../constants/ApiStrings";
 import ImgSlider from './ImgSlider';
-import FrontPageSearchBar from './FrontPageSearchBar';
+//import FrontPageSearchBar from './FrontPageSearchBar';
 
 
 
@@ -10,6 +10,9 @@ function FetchHotels() {
     const [apiData, setApiData] = useState([]);
     const [loadData, setLoadData] = useState(true);
     const [error, setError] = useState(null);
+    const [searchData, setSearchData] = useState('');
+    const [filteredItems, setFilteredItems] = ([])
+
 
     useEffect(function () {
         async function getData() {
@@ -47,17 +50,22 @@ function FetchHotels() {
         return <div>ERROR: An error occured</div>;
     }
 
+    const filteredHotels = apiData.filter(filteredHotel => {
+        return filteredHotel.Name.toLowerCase().includes(searchData.toLocaleLowerCase())
+    })
     return (
-        <>
-            <FrontPageSearchBar names={apiData} />
-            {apiData.map(hotel => {
+        <div>
+            <input type="search" placeholder="search" onChange={(e) => setSearchData(e.target.value)} ></input>
+
+            {filteredHotels.map(hotel => {
                 return <div className="frontpage_flexcontainer" key={hotel.id}>
                     <h2>Hotel {hotel.Name}</h2>
                     <p>{hotel.description}</p>
                     <ImgSlider images={hotel.img} />
                 </div>
             })}
-        </>
+
+        </div>
     )
 };
 export default FetchHotels
--- src/components/frontpage/FrontPageSearchBar.js
+++ src/components/frontpage/FrontPageSearchBar.js
@@ -1,19 +1,17 @@
-import React from 'react'
+/*import React from 'react'
 
 function FrontPageSearchBar(props) {
     console.log(props.names)
     function searchHandler(e) {
         let userInput = e.event.target.value;
         let filteredItems = [];
+
     }
     return (
         <div>
-            <input type="text" placeholder="search" onChange={searchHandler} />
-            {props.names.filter(props === props.Name).map(filteredHotels => (
-                <p>{filteredHotels}</p>
-            ))}
+
         </div>
     )
 }
 
-export default FrontPageSearchBar
+export default FrontPageSearchBar*/
